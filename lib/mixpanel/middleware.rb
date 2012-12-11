dbb29fa3faf9524f240c98a3dd18c83c4caeeb94
Michael Hollander <mich45@gmail.com>
2012-12-10 17:28:59 -0800

    Update lib/mixpanel/middleware.rb
    Added alias method to script

--- lib/mixpanel/middleware.rb
+++ lib/mixpanel/middleware.rb
@@ -85,7 +85,7 @@ module Mixpanel
           var c=b.split(".");2==c.length&&(a=a[c[0]],b=c[1]);a[b]=function(){a.push([b].concat(
           Array.prototype.slice.call(arguments,0)))}}var g=a;"undefined"!==typeof f?g=a[f]=[]:
           f="mixpanel";g.people=g.people||[];h=['disable','track','track_pageview','track_links',
-          'track_forms','register','register_once','unregister','identify','name_tag',
+          'track_forms','register','register_once','unregister','identify','alias','name_tag',
           'set_config','people.identify','people.set','people.increment'];for(e=0;e<h.length;e++)d(g,h[e]);
           a._i.push([b,c,f])};a.__SV=1.1;})(document,window.mixpanel||[]);
 
